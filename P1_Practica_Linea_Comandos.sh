4a3545213ec9a3d15be63562b48a45df6bf1154d
Coriantum <eliasrey12@gmail.com>
2021-12-02 15:59:50 +0100

    Actualizacion de la practica: Todo terminado excepto aquellas que no se solucionar

--- P1_Practica_Linea_Comandos.sh
+++ P1_Practica_Linea_Comandos.sh
@@ -47,7 +47,8 @@ pwd
 ls -i
 
 #16
-rm PRUEBA /*     O   rm -rf PRUEBA /*
+rm PRUEBA /*     
+//Tambien puede ser   rm -rf PRUEBA /*
 
 #17
 mkdir PRUEBA
@@ -135,3 +136,188 @@ rm -rf dir2/fich1
 rm -r *
 
 #41
+mkdir PRUEBA/dir1
+mkdir PRUEBA/dir2
+ls -l
+
+#42
+chmod 555 dir2 
+
+#43
+chmod 551 dir2
+
+#44
+ls -l 
+
+#45
+mkdir dir2/dir21 no se puede hacer,tengo solo permisos de lectura
+
+#46
+chmod 751 dir2
+mkdir dir2/dir21
+
+#47
+ls -l dir2
+
+#48
+cd..
+cd dir3
+ls -R
+
+#49
+ls -lR
+
+#50
+reboot
+
+#51
+mkdir dira
+mkdir dirb
+mkdir dirc
+mkdir dird
+
+#52
+ls -lectura
+
+#53
+touch uno
+chmod ar uno
+ls -l uno
+rm uno
+
+#54 --> Preguntar
+chmod= dir2 
+chmod o=rwx dir2
+
+#55 -- Preguntar si esta bien
+mkdir carpeta1
+chmod u=rwx,g=,o= carpeta1
+touch carpeta1/fich1 
+touch carpeta1/fich2
+chmod = carpeta1/fich1
+chmod a=rw carpeta1/fich1
+chmod = carpeta1/fich2
+chmod u= rw,g=rw,o= carpeta2/fich1
+chmod u= rw,g=,o=r carpeta2/fich2
+
+//fich2
+mkdir carpeta2
+chmod u=rxw,g=rx,o= carpeta2
+touch carpeta2/file1
+touch carpeta2/file2
+chmod = carpeta2/file1 
+chmod u= rw,g=rw carpeta2/file1
+chmod = carpeta2/file2
+chmod u= rw,g=r carpeta2/file2
+
+#56
+ls -lR
+
+#57
+mkdir correo
+mkdir fuentes
+
+#58
+cd fuentes
+mkdir dir1
+mkdir dir2
+mkdir dir3
+
+#59
+mkdir ../correo/menus
+
+#60
+cd /$HOME
+rmdir /*[1]
+
+#61 ?
+find ../dev/tty2
+
+#62
+ls -l /dev/tt*
+
+#63 ?
+find /usr/bin 
+
+#64
+find /*
+
+#65
+
+
+#66
+
+#67
+
+#68
+
+#69
+mkdir uno
+chmod u=rw,g=rw,o= uno
+
+#70
+mkdir uno/uno1
+chmod u=rwx,g= ,o=w uno/uno1
+
+#71
+
+
+#72 ?
+who -H
+cat > Buenos_dias
+cat >> 
+
+#73 -->Intuyo que de por si,los archivos tienen tamaño 0 al ser creados
+touch archivo_tamaño_0
+
+#74
+cat /etc/motd
+
+#75
+
+#76 --> No comprendo la funcion find
+mkdir carpeta
+chmod ar carpeta
+find 
+
+#77
+find 
+
+#78
+
+#79
+
+#80
+time who -H
+
+#81
+ps -U root -u root u
+
+#82
+
+#83 incompleto
+echo "'date+'" >> nuevaTrayectoria
+
+#84
+ps aux
+
+#85 ?
+uptime -p
+
+#86
+ps aux
+
+#87
+cat /etc/passwd | wc -l
+
+#88
+cat /etc/passwd | grep bash
+
+#89
+who -q
+
+#90
+
+
+#91
+cat texto90
